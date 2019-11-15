17abdd8faf1f7c9cdfca1fbda6e5f1fd32dab8f2
Darrik Moberg <>
2019-11-14 16:08:56 -0800

    feature: lets theme take options
    This way it works as a proper theme.

--- gatsby-theme-lifelong-wp-source/gatsby-config.js
+++ gatsby-theme-lifelong-wp-source/gatsby-config.js
@@ -1,14 +1,12 @@
-// require dotenv so that it's useable.
-require("dotenv").config()
-module.exports = {
+module.exports = ({basePath, wordPressUrl}) => ({
     plugins: [
         {
             resolve: "gatsby-source-graphql",
             options: {
                 typeName: 'LifeLongWordPress',
                 fieldName: 'lifeLongWordPress',
-                url: `${process.env.WP_BASE_URL}/graphql`
+                url: `${wordPressUrl}/graphql`
             }
         }
     ]
-}
+})
--- gatsby-theme-lifelong-wp-source/gatsby-node.js
+++ gatsby-theme-lifelong-wp-source/gatsby-node.js
@@ -1,4 +1,4 @@
-exports.createPages = async function ({ actions, graphql}) {
+exports.createPages = async function ({ actions, graphql, reporter}, options) {
     const { data } = await graphql(`
         query {
             lifeLongWordPress {
@@ -17,13 +17,19 @@ exports.createPages = async function ({ actions, graphql}) {
             
         }
     `)
-
+    if(data.errors) {
+      reporter.panic("Error loading pages from WordPress.", result.errors)
+    }
+    let basePath = options.basePath || "/"
+    if(basePath.slice(-1) !== "/") {
+     basePath = basePath.concat("/")
+    }
     data.lifeLongWordPress.pages.nodes.forEach(page => {
         const slug = page.slug;
         const id = page.id
         if(page.base_page_template_data && page.base_page_template_data.mainContent) {
             actions.createPage({
-                path: slug,
+                path: basePath.concat(slug),
                 component: require.resolve('./src/templates/wp-base-template.js'),
                 context: {id: id}
             })
